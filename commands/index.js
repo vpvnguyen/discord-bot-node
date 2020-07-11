e3461fea26e5e06418be68a6fd9313505a584c6d
vpvnguyen <vpvnguyen@gmail.com>
2020-07-10 17:31:49 -0700

    kill bot process

--- commands/index.js
+++ commands/index.js
@@ -7,11 +7,26 @@ const commands = {
   stupidBot: require("./self/stupidBot"),
   // covid
   covid: require("./covid/covid"),
+  // commands
+  commands: {
+    name: "!commands",
+    description: () => `[${commands.commands.name}] - List of commands`,
+    execute(msg, args) {
+      msg.reply("Here is what I can do:");
+      msg.channel.send("----------------");
+      msg.channel.send(ListOfCommands());
+      msg.channel.send("----------------");
+      msg.channel.send("Try typing one of these commands!");
+    },
+  },
 };
 
-console.log(`
-    All Commands
-    ${JSON.stringify(commands)}
-`);
+const ListOfCommands = () => {
+  const commandKeys = Object.keys(commands);
+  const commandName = commandKeys.map((keys) => {
+    return commands[keys].name;
+  });
+  return commandName.sort();
+};
 
 module.exports = commands;
