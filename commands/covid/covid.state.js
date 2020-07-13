e29e6172f65a0beead35c023b28cdbc0ba9573e2
vpvnguyen <vpvnguyen@gmail.com>
2020-07-12 21:06:27 -0700

    fixed executing async message

--- commands/covid/covid.state.js
+++ commands/covid/covid.state.js
@@ -3,13 +3,20 @@ const novelCovidApi = require("../../utils/novelcovid.api");
 const covidState = {
   name: "!covidca",
   description: "Covid data in California",
-  execute(msg, args) {
-    const getData = async () => {
+  execute: async (msg, args) => {
+    try {
       const response = await novelCovidApi.state("California");
       console.log(response);
-      await msg.channel.send(response);
-    };
-    getData();
+      await msg.channel.send([
+        `${response.state}`,
+        `Total Cases: ${response.cases}`,
+        `Today's Cases: ${response.todayCases}`,
+        `Today's Deaths: ${response.todayDeaths}`,
+        `Total Deaths: ${response.deaths}`,
+      ]);
+    } catch (error) {
+      console.error(error.message);
+    }
   },
 };
 
