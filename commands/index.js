8598ae9b2c1164c3d93c939817f488d11a5c0bed
vpvnguyen <vpvnguyen@gmail.com>
2020-07-14 09:58:24 -0700

    provided message layout to data being sent to channel

--- commands/index.js
+++ commands/index.js
@@ -1,3 +1,5 @@
+const messageLayout = require("../utils/layout/message.layout");
+
 const commands = {
   // self
   self: require("./self/self"),
@@ -7,31 +9,28 @@ const commands = {
   stupidBot: require("./self/stupidBot"),
   // covid
   covid: require("./covid/covid"),
-  //   covidCountry: require("./covid/covid.country"),
-  //   covidStates: require("./covid/covid.states"),
-  //   covidState: require("./covid/covid.state"),
   // commands
   commands: {
     name: "!commands",
     description: () => `[${commands.commands.name}] - List of commands`,
     execute(msg, args) {
       msg.reply("Here is what I can do:");
-      msg.channel.send("----------------");
-      msg.channel.send(ListOfCommands());
-      msg.channel.send("----------------");
+      const listOfCommands = getListOfCommands();
+      const formattedMessage = messageLayout(listOfCommands);
+      msg.channel.send(formattedMessage);
       msg.channel.send("Try typing one of these commands!");
     },
   },
 };
 
-const ListOfCommands = () => {
+const getListOfCommands = () => {
   const commandKeys = Object.keys(commands);
-  const commandName = commandKeys.map((keys) => {
+  const commandNames = commandKeys.map((keys) => {
     return commands[keys].name;
   });
 
-  const sortCommandNames = commandName.sort();
-  return [...sortCommandNames];
+  const sortCommandNames = commandNames.sort();
+  return sortCommandNames;
 };
 
 module.exports = commands;
