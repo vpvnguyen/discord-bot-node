ccd89f0f6da7f2a1db9c4dc2b4a57282de3f6b39
vpvnguyen <vpvnguyen@gmail.com>
2020-07-14 20:52:33 -0700

    embed messages using discordjs; set embed example for reference

--- commands/index.js
+++ commands/index.js
@@ -1,4 +1,4 @@
-const messageLayout = require("../utils/layout/message.layout");
+const Discord = require("discord.js");
 
 const commands = {
   // self
@@ -16,9 +16,45 @@ const commands = {
     execute(msg, args) {
       msg.reply("Here is what I can do:");
       const listOfCommands = getListOfCommands();
-      const formattedMessage = messageLayout(listOfCommands);
-      msg.channel.send(formattedMessage);
+      msg.channel.send(listOfCommands);
       msg.channel.send("Try typing one of these commands!");
+    },
+  },
+  // embed example
+  embed: {
+    name: "!embed",
+    execute(msg, args) {
+      const exampleEmbed = new Discord.MessageEmbed()
+        .setColor("#76D7C4")
+        .setTitle("Title")
+        .setURL("https://discord.js.org/")
+        .setAuthor(
+          "Author name",
+          "https://i.imgur.com/wSTFkRM.png",
+          "https://discord.js.org"
+        )
+        .setDescription("Some description here")
+        .setThumbnail("https://i.imgur.com/wSTFkRM.png")
+        .addFields(
+          { name: "Regular field title", value: "Regular value here" },
+          { name: "\u200B", value: "\u200B" },
+          {
+            name: "Inline field title",
+            value: "Inline value here",
+            inline: true,
+          },
+          {
+            name: "Inline field title 2",
+            value: "Inline value here 2",
+            inline: true,
+          }
+        )
+        .addField("Inline field title 3", "Inline value here 3", true)
+        .setImage("https://i.imgur.com/wSTFkRM.png")
+        .setTimestamp()
+        .setFooter("Footer text here", "https://i.imgur.com/wSTFkRM.png");
+
+      msg.channel.send(exampleEmbed);
     },
   },
 };
