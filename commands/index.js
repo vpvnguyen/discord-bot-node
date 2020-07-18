7549c7f4a7ae950c28853f1775345a7acbab5f70
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-18 15:27:53 -0700

    create layout and embed message with commands

--- commands/index.js
+++ commands/index.js
@@ -1,4 +1,16 @@
-const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const getListOfCommands = require("../utils/getListOfCommands");
+
+const layout = {
+  author: "github.com/vpvnguyen",
+  theme: `#6897bb`,
+  footer: () => layout.author,
+  newLine: {
+    name: "\u200B",
+    value: "\u200B",
+    inline: true,
+  },
+};
 
 const commands = {
   // self
@@ -12,61 +24,26 @@ const commands = {
   // commands
   commands: {
     name: "!commands",
-    description: () => `[${commands.commands.name}] - List of commands`,
+    description: "List of commands",
     execute(msg, args) {
-      msg.reply("Here is what I can do:");
-      const listOfCommands = getListOfCommands();
-      msg.channel.send(listOfCommands);
-      msg.channel.send("Try typing one of these commands!");
+      const listOfCommands = getListOfCommands(commands);
+
+      const embededMessage = new MessageEmbed()
+        .setColor(layout.theme)
+        .setDescription(`Here is what I can do:`)
+        .addFields({
+          name: "\u200B",
+          value: listOfCommands.map((command) => `${command.name}`),
+        })
+        .addFields({
+          name: "\u200B",
+          value: "Try typing in one of these commands!\nExample: `!covid`",
+        })
+        .setFooter(layout.footer());
+
+      msg.channel.send(embededMessage);
     },
   },
-  // embed example
-  embed: {
-    name: "!embed",
-    execute(msg, args) {
-      const exampleEmbed = new Discord.MessageEmbed()
-        .setColor("#76D7C4")
-        .setTitle("Title")
-        .setURL("https://discord.js.org/")
-        .setAuthor(
-          "Author name",
-          "https://i.imgur.com/wSTFkRM.png",
-          "https://discord.js.org"
-        )
-        .setDescription("Some description here")
-        .setThumbnail("https://i.imgur.com/wSTFkRM.png")
-        .addFields(
-          { name: "Regular field title", value: "Regular value here" },
-          { name: "\u200B", value: "\u200B" },
-          {
-            name: "Inline field title",
-            value: "Inline value here",
-            inline: true,
-          },
-          {
-            name: "Inline field title 2",
-            value: "Inline value here 2",
-            inline: true,
-          }
-        )
-        .addField("Inline field title 3", "Inline value here 3", true)
-        .setImage("https://i.imgur.com/wSTFkRM.png")
-        .setTimestamp()
-        .setFooter("Footer text here", "https://i.imgur.com/wSTFkRM.png");
-
-      msg.channel.send(exampleEmbed);
-    },
-  },
-};
-
-const getListOfCommands = () => {
-  const commandKeys = Object.keys(commands);
-  const commandNames = commandKeys.map((keys) => {
-    return commands[keys].name;
-  });
-
-  const sortCommandNames = commandNames.sort();
-  return sortCommandNames;
 };
 
 module.exports = commands;
