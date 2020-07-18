c67330c5d1435ff3c37b1660ed99a497cd70616d
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-18 15:29:52 -0700

    generate list of command names, descriptions, args

---
+++ utils/getListOfCommands.js
@@ -0,0 +1,17 @@
+const getListOfCommands = (parentObject) => {
+  const commandKeys = Object.keys(parentObject);
+  const commandList = commandKeys.map((keys) => {
+    return {
+      name: parentObject[keys].name,
+      description: parentObject[keys].description
+        ? parentObject[keys].description
+        : "",
+      args: parentObject[keys].args ? parentObject[keys].args : "",
+    };
+  });
+
+  const sortCommandList = commandList.sort();
+  return sortCommandList;
+};
+
+module.exports = getListOfCommands;
