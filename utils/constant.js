87af03fa0b51bf11f3a878950d4b656c5451f986
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-19 00:27:08 -0700

    set theme, author, and embeded styling

---
+++ utils/constant.js
@@ -0,0 +1,19 @@
+const constant = {
+  theme: {
+    default: "#6897bb",
+    covid: "#EC7063",
+  },
+  author: "github.com/vpvnguyen",
+  inlineSpace: {
+    name: "\u200B",
+    value: "\u200B",
+    inline: true,
+  },
+  newLine: {
+    name: "\u200B",
+    value: "\u200B",
+    inline: false,
+  },
+};
+
+module.exports = constant;
