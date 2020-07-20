67e161558f79b8f3600f661da2bc24ea58ff846d
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-19 19:48:22 -0700

    add link to project

--- commands/self/whoAmI.js
+++ commands/self/whoAmI.js
@@ -2,7 +2,9 @@ const whoAmI = {
   name: "!whoami",
   description: "I am, who I say I am",
   execute(msg, args) {
-    msg.reply("I am sugoisauce's bot. Beep Boop.");
+    msg.reply(
+      "I am sugoisauce's bot. Beep Boop.\nVisit github.com/vpvnguyen/discord-bot-node to help contribute!"
+    );
   },
 };
 
