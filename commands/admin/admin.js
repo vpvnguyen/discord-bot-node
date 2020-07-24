53b0967643ed7c3abe7c23df0a97eea7db817f23
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-23 19:42:01 -0700

    test connection to api server

--- commands/admin/admin.js
+++ commands/admin/admin.js
@@ -1,20 +1,45 @@
 require("dotenv").config();
 const role = require("../../config/role");
+const axios = require("axios");
+const { response } = require("express");
+
+const apiServer = {
+  messageHistory: async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/message-history"
+      );
+      return response.data.message;
+    } catch (error) {
+      console.error(error.message);
+    }
+  },
+};
 
 const admin = {
   name: "!admin",
   description: "Administrative commands",
-  execute(msg, args) {
-    // check users role; refactor permission function
-    if (
-      !role.admin.includes(msg.author.username) ||
-      msg.author.username !== process.env.ADMIN
-    )
-      return msg.reply("Denied.");
+  execute: async (msg, args) => {
+    try {
+      // check users role; refactor permission function
+      if (
+        !role.admin.includes(msg.author.username) ||
+        msg.author.username !== process.env.ADMIN
+      )
+        return msg.reply("Denied.");
 
-    console.log("\n--msg.member--");
-    console.log(msg.member);
-    msg.reply("Admin commands granted");
+      await msg.reply("Admin commands granted");
+      const response = await axios.get("http://localhost:5000/");
+      await msg.channel.send(response.data.message);
+
+      const history = await apiServer.messageHistory();
+      await msg.channel.send(history);
+    } catch (error) {
+      console.error(error.message);
+      await msg.channel.send(
+        "There was an issue connecting to the API server."
+      );
+    }
   },
 };
 
