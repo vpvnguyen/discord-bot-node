ed50822ebae3836deb0c807b3510ab4cda40e0c4
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-23 18:58:15 -0700

    add role check

--- commands/admin/admin.js
+++ commands/admin/admin.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
+const role = require("../../config/role");
 
 const admin = {
   name: "!admin",
   description: "Administrative commands",
   execute(msg, args) {
-    // check users role
-    if (msg.author.username !== process.env.ADMIN) return msg.reply("Denied.");
+    // check users role; refactor permission function
+    if (
+      !role.admin.includes(msg.author.username) ||
+      msg.author.username !== process.env.ADMIN
+    )
+      return msg.reply("Denied.");
 
     console.log("\n--msg.member--");
     console.log(msg.member);
-    msg.reply("admin commands");
+    msg.reply("Admin commands granted");
   },
 };
 
