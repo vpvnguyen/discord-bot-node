4e140889cf5deaa40183c5f692364a3c2990abb5
vpvnguyen <vpvnguyen@gmail.com>
2020-07-24 21:33:22 -0700

    get home and message-history routes from api server

--- commands/admin/admin.js
+++ commands/admin/admin.js
@@ -1,18 +1,36 @@
 require("dotenv").config();
-const role = require("../../config/role");
+const { MessageEmbed } = require("discord.js");
 const axios = require("axios");
-const { response } = require("express");
+
+const role = require("../../config/role");
+const checkPermissions = require("../../utils/checkPermissions");
 
 const apiServer = {
-  messageHistory: async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:5000/api/message-history"
-      );
-      return response.data.message;
-    } catch (error) {
-      console.error(error.message);
-    }
+  home: {
+    name: "home",
+    args: "home",
+    home: async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/");
+        return response.data.message;
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+  },
+  messageHistory: {
+    name: "messageHistory",
+    args: "messageHistory",
+    messageHistory: async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/message-history"
+        );
+        return response.data.message;
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
   },
 };
 
@@ -29,10 +47,11 @@ const admin = {
         return msg.reply("Denied.");
 
       await msg.reply("Admin commands granted");
-      const response = await axios.get("http://localhost:5000/");
-      await msg.channel.send(response.data.message);
 
+      const home = await apiServer.home();
       const history = await apiServer.messageHistory();
+
+      await msg.channel.send(home);
       await msg.channel.send(history);
     } catch (error) {
       console.error(error.message);
