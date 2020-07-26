e67ea78790db9900e42d6d5270532a601f1b32ce
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-26 12:07:05 -0700

    create db credentials

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,10 +1,12 @@
+require("dotenv").config({ path: "../../.env" });
 const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
+  password: process.env.DB_PASSWORD,
   port: 5432,
-  database: "discord_bot",
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
