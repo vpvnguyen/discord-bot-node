f2602b9db91ac126f596153d2070d02d9e51e9b0
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-27 23:05:04 -0700

    get user's messages and if messages contains a link

--- server/controller/messageHistory.controller.js
+++ server/controller/messageHistory.controller.js
@@ -2,6 +2,17 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../config/db.config");
 
+router.get("/messages", async (req, res) => {
+  try {
+    const messages = await pool.query("SELECT * FROM messages");
+    console.log(messages.rows);
+    res.status(200).json(messages.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Issue retrieving all messages" });
+  }
+});
+
 router.get("/user/:id/messages", async (req, res) => {
   try {
     const { id } = req.params;
@@ -9,6 +20,47 @@ router.get("/user/:id/messages", async (req, res) => {
       "SELECT * FROM messages WHERE user_id = $1",
       [id]
     );
+    res.status(200).json(messages.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Issue retrieving user messages" });
+  }
+});
+
+router.get("/curses", async (req, res) => {
+  try {
+    const messages = await pool.query(
+      "SELECT * FROM messages WHERE amount_curse > 0"
+    );
+    console.log(messages.rows);
+    res.status(200).json(messages.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Issue retrieving user messages" });
+  }
+});
+
+router.get("/user/:id/curses", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const messages = await pool.query(
+      "SELECT * FROM messages WHERE user_id = $1 AND amount_curse > 0",
+      [id]
+    );
+    console.log(messages.rows);
+    res.status(200).json(messages.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Issue retrieving user messages" });
+  }
+});
+
+router.get("/links", async (req, res) => {
+  try {
+    const messages = await pool.query(
+      "SELECT * FROM messages WHERE has_link = true"
+    );
+    console.log(messages.rows);
     res.status(200).json(messages.rows);
   } catch (error) {
     console.error(error.message);
