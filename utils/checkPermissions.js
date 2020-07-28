ace5cedbf7dd58a165a100565d6dcd8741213b08
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-27 23:27:20 -0700

    get user's permission

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,5 +1,12 @@
-const checkPermissions = () => {
-  console.log("checkPermissions");
+const axios = require("axios");
+
+const checkPermissions = async (msg) => {
+  const [username, discriminator] = msg.split("#");
+  const getUserRole = await axios.get(
+    `http://localhost:5000/api/user/${username}/${discriminator}`
+  );
+  const userRole = getUserRole.data[0].role;
+  console.log(userRole);
 };
 
 module.exports = checkPermissions;
