e1112db9744063186b66ec795de3c5131a4d2347
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-27 23:05:32 -0700

    get user by user's tag

--- server/controller/users.controller.js
+++ server/controller/users.controller.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../config/db.config");
-const axios = require("axios");
 
 router.get("/users", async (req, res) => {
   try {
     const users = await pool.query("SELECT * FROM users");
-    console.log(users.rows);
     res.status(200).json(users.rows);
   } catch (error) {
     console.error(error.message);
@@ -20,6 +18,21 @@ router.get("/user/:id", async (req, res) => {
     const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [
       id,
     ]);
+    res.status(200).json(user.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Issue retrieving user" });
+  }
+});
+
+router.get("/user/:username/:discriminator", async (req, res) => {
+  try {
+    const { username, discriminator } = req.params;
+    const tag = `#${discriminator}`;
+    const user = await pool.query(
+      "SELECT * FROM users WHERE username = $1 AND discriminator = $2",
+      [username, tag]
+    );
     res.status(200).json(user.rows);
   } catch (error) {
     console.error(error.message);
