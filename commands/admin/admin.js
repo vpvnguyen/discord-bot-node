5da84a51f5c470c7c8464f7da7419bb3a1a08c2b
vpvnguyen <vpvnguyen@gmail.com>
2020-07-28 20:36:38 -0700

    check if user is admin

--- commands/admin/admin.js
+++ commands/admin/admin.js
@@ -1,63 +1,25 @@
 require("dotenv").config();
-const { MessageEmbed } = require("discord.js");
-const axios = require("axios");
-
-const role = require("../../config/role");
-const checkPermissions = require("../../utils/checkPermissions");
-
-const apiServer = {
-  home: {
-    name: "home",
-    args: "home",
-    home: async () => {
-      try {
-        const response = await axios.get("http://localhost:5000/");
-        return response.data.message;
-      } catch (error) {
-        console.error(error.message);
-      }
-    },
-  },
-  messageHistory: {
-    name: "messageHistory",
-    args: "messageHistory",
-    messageHistory: async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/message-history"
-        );
-        return response.data.message;
-      } catch (error) {
-        console.error(error.message);
-      }
-    },
-  },
-};
+const { checkRole } = require("../../utils/api/users.api");
+const { home } = require("../../utils/api/home.api");
 
 const admin = {
   name: "!admin",
   description: "Administrative commands",
   execute: async (msg, args) => {
     try {
-      // check users role; refactor permission function
-      if (
-        !role.admin.includes(msg.author.username) ||
-        msg.author.username !== process.env.ADMIN
-      )
-        return msg.reply("Denied.");
+      const { username, discriminator } = msg.author;
+      const userRole = await checkRole(username, discriminator);
+
+      if (userRole !== "admin") return msg.reply("Denied.");
 
       await msg.reply("Admin commands granted");
 
-      const home = await apiServer.home();
-      const history = await apiServer.messageHistory();
+      const homeMessage = await home();
 
-      await msg.channel.send(home);
-      await msg.channel.send(history);
+      await msg.channel.send(homeMessage);
     } catch (error) {
       console.error(error.message);
-      await msg.channel.send(
-        "There was an issue connecting to the API server."
-      );
+      await msg.reply("There was an issue connecting to the API server.");
     }
   },
 };
