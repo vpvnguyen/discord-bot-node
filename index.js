17bfd1885b509b5e9d749ab7dc20ce989aab35f8
Vincent Nguyen <vpvnguyen@gmail.com>
2020-08-01 13:20:17 -0700

    refactored handling commands and message history

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
-const { Client, Collection, MessageEmbed } = require("discord.js");
-const {
-  doesNotContainCommand,
-  defineInput,
-  commandDoesNotExist,
-} = require("./utils/messages.util");
+const { Client, Collection } = require("discord.js");
+const { messageContent } = require("./utils/constant");
 const botCommands = require("./commands");
 
 const bot = new Client();
@@ -19,13 +15,30 @@ bot.on("ready", () => {
 });
 
 bot.on("message", async (msg) => {
-  // console.log(msg);
-  // recorder
-  if (doesNotContainCommand(msg)) return;
+  if (msg.content.startsWith(messageContent.prefix) && !msg.author.bot)
+    return checkCommands(msg);
 
-  const { command, args } = defineInput(msg);
+  if (!msg.author.bot) return record(msg);
+});
 
-  if (commandDoesNotExist(bot, command))
+const record = (msg) => {
+  let hasLink = false;
+  let amountCurse = 0;
+
+  if (msg.content.match(messageContent.url)) hasLink = true;
+
+  if (hasLink || amountCurse > 0) {
+    // pass msg into records
+    return console.log("recording message");
+  }
+};
+
+const checkCommands = (msg) => {
+  console.log("check commands");
+  const args = msg.content.split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!bot.commands.has(command))
     return msg.reply(
       "Huh? You rang? No comprende...\nType `!commands` to see what I can do."
     );
@@ -36,6 +49,6 @@ bot.on("message", async (msg) => {
     console.error("Bot on message error:", error.message);
     msg.reply("There was an error trying to execute that command!");
   }
-});
+};
 
 bot.login(process.env.DISCORD_BOT_TOKEN);
