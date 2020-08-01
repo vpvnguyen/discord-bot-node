2622e5bc4e01b4309a17fa24dc01dd7ada4d8969
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-31 18:56:15 -0700

    refactored message helper into a utility

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const { Client, Collection, MessageEmbed } = require("discord.js");
+const {
+  doesNotContainCommand,
+  defineInput,
+  commandDoesNotExist,
+} = require("./utils/messages.util");
 const botCommands = require("./commands");
-const dayjs = require("dayjs");
 
 const bot = new Client();
 bot.commands = new Collection();
-let links = [];
 
 bot.on("ready", () => {
   Object.keys(botCommands).map((key) =>
@@ -18,14 +21,11 @@ bot.on("ready", () => {
 bot.on("message", async (msg) => {
   // console.log(msg);
   // recorder
-  saveLinks(msg);
   if (doesNotContainCommand(msg)) return;
 
   const { command, args } = defineInput(msg);
-  if (command === "!accesslinks") return await accessLinks(msg);
-  if (command === "!kill") return exit(msg);
 
-  if (commandDoesNotExist(command))
+  if (commandDoesNotExist(bot, command))
     return msg.reply(
       "Huh? You rang? No comprende...\nType `!commands` to see what I can do."
     );
@@ -39,84 +39,3 @@ bot.on("message", async (msg) => {
 });
 
 bot.login(process.env.DISCORD_BOT_TOKEN);
-
-const doesNotContainCommand = (msg) => {
-  const prefix = "!";
-  if (
-    !msg.content.startsWith(prefix) ||
-    msg.author.bot ||
-    msg.author.username === (process.env.DISCORD_BOT_NAME || "sugoi-bot")
-  )
-    return true;
-};
-
-const defineInput = (msg) => {
-  const args = msg.content.split(/ +/);
-  const command = args.shift().toLowerCase();
-  return {
-    command,
-    args,
-  };
-};
-
-const commandDoesNotExist = (command) => {
-  if (!bot.commands.has(command)) return true;
-  return false;
-};
-
-const exit = (msg) => {
-  if (msg.author.bot || msg.author.username !== process.env.ADMIN)
-    return msg.reply("I don't think so");
-
-  let countDown = 5;
-
-  msg.channel.send(`Shutting down in ${countDown}...`);
-
-  setInterval(async () => {
-    countDown--;
-    if (countDown === 0) {
-      await msg.channel.send("Goodbye.");
-      return process.exit(22);
-    }
-    return msg.channel.send(`${countDown}...`);
-  }, 1000);
-};
-
-const saveLinks = (msg) => {
-  if (
-    msg.content.includes("http" || "https") &&
-    !msg.author.bot &&
-    msg.author.username !== process.env.DISCORD_BOT_NAME
-  ) {
-    links.push({
-      author: msg.author.username,
-      content: msg.content,
-      date: new Date(),
-    });
-  }
-};
-
-const accessLinks = async (msg) => {
-  console.log(links);
-  if (links.length === 0) return msg.reply("There are no links");
-  const embededMessage = new MessageEmbed()
-    .setColor("#32a8a8")
-    .setDescription(
-      `There are [${links.length}] link(s) recorded since ${dayjs(
-        links[0].date
-      ).format("MM-DD-YYYY hh:mma")}:`
-    )
-    .addFields(
-      links.map((value) => {
-        return {
-          name: `${value.author} | ${dayjs(value.date).format(
-            "MM-DD-YYYY hh:mma"
-          )}`,
-          value: `${value.content}`,
-        };
-      })
-    )
-    .setFooter("github.com/vpvnguyen");
-
-  await msg.channel.send(embededMessage);
-};
