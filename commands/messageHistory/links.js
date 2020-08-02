1f736865e9b632005cf4a0de88fccfd41c32b7a7
vpvnguyen <vpvnguyen@gmail.com>
2020-08-02 15:05:35 -0700

    embed links into be sent to channel

--- commands/messageHistory/links.js
+++ commands/messageHistory/links.js
@@ -1,18 +1,38 @@
 const { MessageEmbed } = require("discord.js");
+const dayjs = require("dayjs");
 const { embedLayout } = require("../../utils/constant");
+const { getLinksByChannel } = require("../../utils/api/messages.api");
 
 const links = {
   name: "!links",
-  description: "Retrieve message history of links",
+  description: "Retrieve channel's message history containing links",
   execute: async (msg, args) => {
+    const channelLinks = await getLinksByChannel(msg.channel.guild.id);
+
+    if (channelLinks.length === 0)
+      return msg.reply(
+        `There are no links found for ${msg.channel.guild.name}`
+      );
+
     const embedMessage = new MessageEmbed()
-      .setColor(embedLayout.theme.default)
-      .setDescription(`${msg.channel.guild.name} link history`)
-      .addFields({
-        name: "Get Link",
-        value: "Get Link",
-      })
-      .setFooter(embedLayout.author);
+      .setColor(embedLayout.theme.history)
+      .setThumbnail(
+        embedLayout.guild.getIcon(msg.channel.guild.id, msg.channel.guild.icon)
+      )
+      .setDescription(
+        `Found ${channelLinks.length} link(s) from ${msg.channel.guild.name}`
+      )
+      .addFields(
+        channelLinks.map((value) => {
+          return {
+            name: `${value.username}#${value.discriminator} | ${dayjs(
+              value.date
+            ).format("MM-DD-YYYY hh:mma")}`,
+            value: `${value.message}`,
+          };
+        })
+      )
+      .setFooter(`Timezone is GMT | ${embedLayout.author}`);
 
     await msg.channel.send(embedMessage);
   },
