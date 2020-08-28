2cc03fc76957408cf384e86c9e232aacc6d73916
vpvnguyen <vpvnguyen@gmail.com>
2020-08-27 17:43:37 -0700

    WIP add todo items

--- server/controller/mentions.controller.js
+++ server/controller/mentions.controller.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+// get all mentions
 router.get("/mentions", (req, res) => {
   console.log("get mentions");
   res.status(200).json({ message: "get mentions" });
 });
 
+// save mention
 router.post("/mentions", (req, res) => {
   console.log("saving mentions");
+  // TODO:
+  // find if author exists in users; create user if does not exist
+  // save mention
   res.status(200).json({ message: "saving mentions" });
 });
 
