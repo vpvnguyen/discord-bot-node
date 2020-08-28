0fc5d3cfaf523afa9d56b5dff683d2c16555d179
vpvnguyen <vpvnguyen@gmail.com>
2020-08-27 17:07:24 -0700

    find user mentioned within message content using regex

--- utils/constant.js
+++ utils/constant.js
@@ -6,6 +6,7 @@ const constant = {
     url: new RegExp(
       /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
     ),
+    mention: new RegExp(/<@[\S]+>/g),
   },
   roles: ["admin", "mod", "user"],
   embedLayout: {
