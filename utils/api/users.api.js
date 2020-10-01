b185ad5fc7fbd570c6da2aabb98af9c201be581d
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-30 17:24:37 -0700

    changed to users routes

--- utils/api/users.api.js
+++ utils/api/users.api.js
@@ -3,42 +3,44 @@ const axios = require("axios");
 const usersApi = {
   users: {
     getAllUsers: async () => {
-      const users = await axios.get(`http://localhost:5000/api/users`);
+      const users = await axios.get(`http://localhost:5000/users`);
       return users.data;
     },
     getUserById: async (userId) => {
-      const user = await axios.get(`http://localhost:5000/api/user/${userId}`);
+      const user = await axios.get(
+        `http://localhost:5000/users/user/${userId}`
+      );
       return user.data[0];
     },
     getUserByUsername: async (username, discriminator) => {
       const user = await axios.get(
-        `http://localhost:5000/api/user/${username}/${discriminator}`
+        `http://localhost:5000/users/user/${username}/${discriminator}`
       );
       return user.data;
     },
     addMe: async (user) => {
       const { id, username, discriminator } = user;
       const currentUser = await axios.post(
-        `http://localhost:5000/api/user/${id}/${username}/${discriminator}`
+        `http://localhost:5000/users/user/${id}/${username}/${discriminator}`
       );
       return currentUser.data[0];
     },
     updateRole: async (userId, role) => {
       const updateRole = await axios.put(
-        `http://localhost:5000/api/user/update-role`,
+        `http://localhost:5000/users/user/update-role`,
         { userId, role }
       );
       return updateRole.data[0];
     },
     deleteByUserId: async (userId) => {
       const deleteUserByUserId = await axios.delete(
-        `http://localhost:5000/api/user/${userId}`
+        `http://localhost:5000/users/user/${userId}`
       );
       return deleteUserByUserId.message;
     },
     checkRole: async (username, discriminator) => {
       const user = await axios.get(
-        `http://localhost:5000/api/user/${username}/${discriminator}`
+        `http://localhost:5000/users/user/${username}/${discriminator}`
       );
       return user.data[0];
     },
